3f3a4f6423e68d163b542bae6470ddc5a81524a2
Tim Jarratt <tim.jarratt@maersk.com>
2024-12-02 16:15:48 +0100

    Version 2 : use country codes instead of states

--- lib/elephant_carpacchio_web/calculation_live.ex
+++ lib/elephant_carpacchio_web/calculation_live.ex
@@ -4,13 +4,13 @@ defmodule ElephantCarpacchioWeb.CalculationLive do
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <.header>Easy Self-Checkout</.header>
+    <.header>Super Easy Energy Transition Store</.header>
 
     <.form for={@form} phx-submit="calculate">
       <.input type="number" field={@form[:count]} label="How many items ?" value={nil} />
       <.input type="number" field={@form[:price]} label="Price per item ?" value={nil} />
 
-      <.input type="select" field={@form[:state]} label="US State" options={~w[California Nevada Texas Alabama Utah]} />
+      <.input type="select" field={@form[:country]} label="Country" options={["Portugal", "India", "United Kingdom", "United States", "France" ]} />
 
       <.button type="submit" class="mt-8">
         Checkout
@@ -36,13 +36,13 @@ defmodule ElephantCarpacchioWeb.CalculationLive do
     result =
       (count * price_per_item)
       |> apply_discount()
-      |> apply_taxes(params["state"])
+      |> apply_taxes(params["country"])
 
     {:noreply, socket |> assign(:result, result)}
   end
 
-  defp apply_taxes(amount, state) do
-    amount * tax_rate(state)
+  defp apply_taxes(amount, country) do
+    amount * tax_rate(country)
   end
 
   defp apply_discount(total) when total > 50_000, do: total * 0.85
@@ -52,9 +52,9 @@ defmodule ElephantCarpacchioWeb.CalculationLive do
   defp apply_discount(total) when total > 1_000, do: total * 0.97
   defp apply_discount(total), do: total
 
-  defp tax_rate("California"), do: 1.0825
-  defp tax_rate("Nevada"), do: 1.08
-  defp tax_rate("Texas"), do: 1.0625
-  defp tax_rate("Alabama"), do: 1.04
-  defp tax_rate("Utah"), do: 1.0685
+  defp tax_rate("France"), do: 1.1125
+  defp tax_rate("Portugal"), do: 1.08
+  defp tax_rate("United Kingdom"), do: 1.0625
+  defp tax_rate("United States"), do: 1.04
+  defp tax_rate("India"), do: 1.0685
 end
